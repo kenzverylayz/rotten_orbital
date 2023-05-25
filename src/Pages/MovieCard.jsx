c511598d5fdf99517a346533b9b96c4cee35298e
Koh Ken Tze <kenzverylayz@MacBook-Pro-7.local>
2023-05-25 13:52:27 +0800

    Commit

--- src/Pages/MovieCard.jsx
+++ src/Pages/MovieCard.jsx
@@ -4,14 +4,12 @@ const MovieCard = ( { movie }) => {
     return (
         <div className='movie'>
                     <div>
-                        <p>{movie.Year}</p>
-                    </div>
-                    <div>
                         <img src={movie.Poster != 'N/A' ? movie.Poster : 'https://via.placeholder.com/400'} alt = {movie.Title}/>
                     </div>
                     <div>
                         <span>{movie.Type}</span>
                         <h3>{movie.Title}</h3>
+                        <p>{movie.Year}</p>
                     </div>
                 </div>
     );
